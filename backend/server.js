2292096d0e95182dfa89ce093a687d8ecacbd419
Asmita Jarhalli <asmitajarhalli123@gmail.com>
2026-06-14 00:02:04 +0530

    MongoDB is added and connected

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,32 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
-const churnRoute = require("./routes/churnRoute");
+const connectDB = require(
+    "./config/db"
+);
+
+const churnRoute = require(
+    "./routes/churnRoute"
+);
 
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/churn", churnRoute);
+app.use(
+    "/api/churn",
+    churnRoute
+);
 
 const PORT = 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server Running On ${PORT}`);
+
+    console.log(
+        `Server Running On ${PORT}`
+    );
 });
